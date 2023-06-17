55a130b3d25ceef043caedf28c3bc9258db4da5c
Abhijit Kumar Mahato <abhijitkr600@gmail.com>
2023-06-17 14:29:56 +0530

    Saving Todo with UserId
    Fixed user ID not saving in database.

--- add_todo.php
+++ add_todo.php
@@ -1,9 +1,17 @@
 <?php
+session_start();
 require_once 'db_connect.php';
 
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    $response = array('success' => false, 'message' => 'User not logged in.');
+    echo json_encode($response);
+    exit();
+}
+
 // Prepare the SQL statement
-$stmt = $mysqli->prepare("INSERT INTO todos (item) VALUES (?)");
-$stmt->bind_param("s", $_POST['item']);
+$stmt = $mysqli->prepare("INSERT INTO todos (user_id, item) VALUES (?, ?)");
+$stmt->bind_param("is", $_SESSION['user_id'], $_POST['item']);
 
 // Execute the statement
 $success = $stmt->execute();
--- login.php
+++ login.php
@@ -1,29 +1,32 @@
 <?php
+session_start();
 require_once 'db_connect.php';
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-  if (!empty($email) && !empty($password)) {
-    $stmt = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $user = $result->fetch_assoc();
+    if (!empty($email) && !empty($password)) {
+        $stmt = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
 
-    if ($user && password_verify($password, $user['password'])) {
-      echo "<script>alert('Login successful!');</script>";
-      echo "<script>window.location.href = 'todolist.php';</script>";
-      exit();
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id']; // Store the user_id in the session
+
+            echo "<script>alert('Login successful!');</script>";
+            echo "<script>window.location.href = 'todolist.php';</script>";
+            exit();
+        } else {
+            echo "<script>alert('Invalid email or password.');</script>";
+        }
+
+        $stmt->close();
+        $mysqli->close();
     } else {
-      echo "<script>alert('Invalid email or password.');</script>";
+        echo "<script>alert('Please fill in all the required fields.');</script>";
     }
-
-    $stmt->close();
-    $mysqli->close();
-  } else {
-    echo "<script>alert('Please fill in all the required fields.');</script>";
-  }
 }
 ?>
